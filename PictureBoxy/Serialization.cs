4b1646418d9b1945dca75bb5b89943ccfd65f018
Falladyn1 <matibati300@gmail.com>
2026-05-21 12:27:16 +0200

    serializer

---
+++ PictureBoxy/Serialization.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.ComponentModel;
+
+namespace PictureBoxy
+{
+    internal class Serialization
+    {
+        public void Save(object o)
+        {
+            Type t = o.GetType();
+            MemberInfo[] members = t.GetMembers();
+
+            foreach (MemberInfo mem in members)
+            {
+                object[] attributes = mem.GetCustomAttributes(true);
+
+                if (attributes.Length != 0 && mem.MemberType==MemberTypes.Field)
+                {
+                    string key = "";
+                    foreach (object attr in attributes)
+                    {
+                        DescriptionAttribute da = attr as DescriptionAttribute;
+                        if (da != null)
+                        {
+                            key = da.Description;
+                        }
+                    }
+                    object ob = mem.ReflectedType.GetField(mem.Name).GetValue(this);
+
+                    if (ob != null)
+                    {
+                        dicField.Add(key, mem.ReflectedType.GetField(mem.Name).GetValue(this).ToString();
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+        }
+
+        // funkcja Save(object o)
+        // Type t = o.GetType()
+        // MembersInfo[] members = t.GetMembers();
+
+    }
+}
